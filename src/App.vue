464da3d0b7c7284043791bb854b127423f98e2b2
clf <esx56487@qq.com>
2018-12-24 17:05:34 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <CubeCore></CubeCore>
+  </div>
+</template>
+
+<script>
+import CubeCore from './components/CubeCore';
+
+export default {
+  name: 'app',
+  components: {
+    CubeCore,
+  },
+};
+</script>
+
+<style>
+html,body{
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    font-size: 60px;
+    user-select:none;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding-top: 20px;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+}
+</style>
